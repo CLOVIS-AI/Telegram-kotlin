5fbc4ea5d261e6bb7f69626b581eed39d501d7da
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-01-22 20:05:08 +0100

    build(gradle): Include the modules in the publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2026, OpenSavvy and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.dokka.DokkaDefaults.pluginsConfiguration
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
@@ -25,7 +42,8 @@ plugins {
 
 dependencies {
 	// List the 'library' projects
-	// library(projects.core)
+	library(projects.entities)
+	library(projects.sdk)
 }
 
 // region Check the users of the project didn't forget to rename the group
